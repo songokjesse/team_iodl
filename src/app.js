44bd7ea6dc08b6f1fb15d7aef572c3104a518a19
jessesongok <jessesongok@gmail.com>
2020-03-03 10:02:08 +0300

    intial commit

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const routes = require('../src/routes');
+const  app = express();
+
+app.use(logger('common'));
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// API ROUTES
+app.use('/api/v1', routes);
+
+//catch 404 and forward to error handler
+app.use((req,res,next)=>{
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use((err,req,res,next)=>{
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: err
+    });
+});
+
+module.exports = app;
